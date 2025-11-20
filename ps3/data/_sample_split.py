aa417f216ac4d360f6e83bcbef0751aeba6a808b
yfoxgit <yasminfox1998@gmail.com>
2025-11-20 14:13:43 +0000

    ex1 complete

--- ps3/data/_sample_split.py
+++ ps3/data/_sample_split.py
@@ -23,25 +23,35 @@ def create_sample_split(df: pd.DataFrame, id_column: str, training_frac: float =
     pd.DataFrame
         Training data with sample column containing train/test split based on IDs.
     """
-    bins = ['train', 'test']
+    # Crate safe copy of dataframe so original is untouched
+    df = df.copy()
 
-    # Convert training fraction to integer
-    training_frac = int(training_frac*100)
+    # Check training fraction input is correct
+    # Convert fraction to number between 0 and 100
+    if training_frac != 0.8 and not (0 <= training_frac <= 1):
+        print('Please input a training fraction between 0 and 1')
+        return
+    else:
+        training_frac = training_frac
+    # Define cut of dataframe based on its length and the training_frac
+    cut = int(len(df)*training_frac)
     
     # Function to hash a value and convert to an integer
     def hash_to_int(value):
-        hash_obj = hashlib.sha256(str(value).encode('utf-8'))
-        hex_digest = hash_obj.hexdigest()
-        return int(hex_digest, 16)  # convert hex string → integer
+        hash_obj = hashlib.sha256(str(value).encode('utf-8')).hexdigest()
+        h_int = int(hash_obj, 16)
+        # ensure the hash follows a uniform distribution [0,1]
+        return h_int/(2**256)
 
     # Create hash column in dataframe
     df['hash'] = df[id_column].apply(hash_to_int)
+    df = df.sort_values('hash')
     
-    # Function to assign split
-    def assign_split(hash_value: int):
-        bucket = hash_value % 100
-        return 'train' if bucket < training_frac else 'test'
+    # Create sample column and apply train/test split
+    df['sample'] = 'test'
+    df.iloc[:cut, df.columns.get_loc('sample')] = 'train'
 
-    df['sample'] = df[id_column].apply(assign_split)
+    # Resort dataframe back to original
+    df = df.sort_index()
 
     return df
